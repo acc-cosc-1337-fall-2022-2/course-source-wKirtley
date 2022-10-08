00cb10b51ba3a173eee975f7ee13e9265d602661
wKirtley <william.kirtley@g.austincc.edu>
2022-10-08 18:52:53 +0000

    hw-05

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,58 @@
-//add include statements
+#include <iostream>
+#include "func.h"
 
-//add function code here
+using namespace std;
+using std::string;
+
+
+
+double get_gc_content(const string& dna)
+{
+    int i=0, count=0;
+    for(i=0; i<dna.size(); i++)
+    {
+    if(dna[i] == 'G' || dna[i] == 'C')
+    {
+        count++;
+    }
+    }
+    return ((double)count)/dna.size();
+
+}
+
+string reverse_string(string dna)
+{
+    string rstr = "";
+    for(int i=dna.length()-1; i>=0; i--)
+    {
+        rstr += dna[i];
+    }
+    return rstr;
+}
+
+string get_dna_compliment(string dna)
+{
+    string compliment = "";
+    dna = reverse_string(dna);
+    for(int i=0; i<dna.length(); i++)
+    {
+        if(dna[i] == 'A')
+        {
+        compliment += 'T';
+        }
+        else if(dna[i] == 'T')
+        {
+        compliment += 'A';
+        }
+        else if(dna[i] == 'G')
+        {
+        compliment += 'C';
+        }
+        else if(dna[i] == 'C')
+        {
+        compliment += 'G';
+        }
+    }
+    return compliment;
+}
+
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,3 @@
-//add function(s) prototype here
+double get_gc_content(const std::string& dna);
+std::string get_dna_compliment(std::string dna);
+std::string reverse_string(std::string dna);
